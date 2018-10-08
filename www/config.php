ba4fa141b6477e08168e165e05fb9a5eba570ce8
Vyacheslav <viacheslav.suprun@yandex.ru>
2018-10-08 21:18:07 +0300

    project

--- www/config.php
+++ www/config.php
@@ -6,6 +6,11 @@ define('DB_NAME', 'WD04-project-suprun');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
+// SITE SETTINGS FOR EMAILS
+define('SITE_NAME', 'личный сайт Вячеслава Супрун');
+define('SITE_EMAIL', 'info@webdew04.com');
+define('ADMIN_EMAIL', 'info@rightblog.ru');
+
 /*Устанавливает путь до корневой директории скрипта по протоколу HTTP*/
 define('HOST', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
 
--- www/index.php
+++ www/index.php
@@ -1,7 +1,12 @@
 <?php
 
+$errors = array();
+$success = array();
+
 require "config.php";
 require "db.php";
+require "libs/functions.php";
+session_start();
 
 /*.......................................
  РОУТЕР
@@ -16,20 +21,52 @@ $uri = explode('?', $uri);
 
 switch ( $uri[0]) {
     case '':
-        include "modules/main/index.php";
+        require ROOT . "modules/main/index.php";
         break;
+
+//::::::::::::::: USERS :::::::::::::::::::::
+
+    case 'login':
+        require ROOT . "modules/login/login.php";
+        break;
+
+    case 'registration':
+        include ROOT . "modules/login/registration.php";
+        break;
+
+    case 'logout':
+        include ROOT . "modules/login/logout.php";
+        break;
+
+    case 'lost-password':
+        include ROOT . "modules/login/lost-password.php";
+        break;
+
+    case 'set-new-password':
+        include ROOT . "modules/login/set-new-password.php";
+        break;
+
+    case 'profile':
+        include ROOT . "modules/profile/index.php";
+        break;
+
+    case 'profile-edit':
+        include ROOT . "modules/profile/edit.php";
+        break;
+
+//::::::::::::::: / USERS :::::::::::::::::::::
         
-    case 'about':
+    /*case 'about':
         include "modules/about/index.php";
         break;
-        
+       
     case 'contacts':
         include "modules/contacts/index.php";
         break;
         
     case 'blog':
         include "modules/blog/index.php";
-        break;
+        break;*/
         
     default:
         include "modules/main/index.php";
